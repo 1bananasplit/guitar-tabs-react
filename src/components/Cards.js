02df2fe5bf842afcffb5b3163be4c91e16cbf30c
Banana split <tboc300@gmail.com>
2022-07-05 10:33:15 +0000

    tentative échouée d'un systeme de rajout de carte par formulaire

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,44 @@
 import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
 import { cardsList } from './cards-list'
-import Add from './Add'
 import '../styles/cards.css'
 
 export default function Cards() {
-    const [cards] = useState(cardsList)
+const [cards, setCards] = useState(cardsList)
+
+// const [authors, setAuthors] = useState('')
+// const [nom, setNom] = useState('')
+// const [link, setLink] = useState('')
+
+// const submit = (e) => {
+//   e.preventDefault()
+
+//   cards.push({
+//     authors: authors,
+//     nom: nom,
+//     link: link,
+//     },)
+
+//   setAuthors('')
+//   setNom('')
+//   setLink('')
+
+//   {console.log(cards)}
+
+// }
+
+// const changeAuthors = (e) => {
+//     console.log(e.target.value)
+//     setAuthors(e.target.value)
+// }
+// const changeNom = (e) => {
+//     console.log(e.target.value)
+//     setNom(e.target.value)
+// }
+// const changeLink = (e) => {
+//     console.log(e.target.value)
+//     setLink(e.target.value)
+// }
 
   return (
     <div className='cards'>
@@ -17,7 +50,13 @@ export default function Cards() {
                </span>   
                 <img src={`https://i.ytimg.com/vi/${e.link}/hq720.jpg`}></img>
            </NavLink>)}
-           {/* <Add /> */}
+
+           {/* <form onSubmit={submit}>
+            <input placeholder="L'auteur" type="text" id='authors' value={authors} onChange={changeAuthors}></input>
+            <input placeholder="Le nom" type="text" id='nom' value={nom} onChange={changeNom}></input>
+            <input placeholder="Le lien" type="text" id='link' value={link} onChange={changeLink}></input>
+            <button></button>
+        </form> */}
     </div>
   )
 }
